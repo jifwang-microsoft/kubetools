e272b94414e089b96e2ce13899f6f7d6464de4d4
Rohit <rjaini@microsoft.com>
2019-05-21 11:22:46 -0700

    fix the wordpress install. (#51)

--- applications/wordpress/install_wordpress_using_helm.sh
+++ applications/wordpress/install_wordpress_using_helm.sh
@@ -29,7 +29,7 @@ echo "Update helm repository..."
 helm repo update
 
 echo "Install wordpress..."
-helm install stable/wordpress --set wordpressSkipInstall=no
+helm install stable/wordpress --set wordpressSkipInstall=false
 
 echo "Done with installation, checking release status..."
 wpRelease=$(helm ls -d -r | grep 'DEPLOYED\(.*\)wordpress' | grep -Eo '^[a-z,-]+')
