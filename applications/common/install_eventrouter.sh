759dfe7c6293a76e03d5201fbc72f1ab0bd5ced6
deaborch <44680211+deaborch@users.noreply.github.com>
2019-06-18 16:39:53 -0700

    foramting fix

--- applications/common/install_eventrouter.sh
+++ applications/common/install_eventrouter.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'    # For error
 GREEN='\033[0;32m'  # For crucial check success
 NC='\033[0m'        # No color, back to normal
 
-echo -e " $(date) [Info] Install Eventrouter to collect the events for all kubernetes deployments"
+echo -e " $(date) [Info] Installing eventrouter to collect the events for all kubernetes deployments"
 
 # Install helm if it is not available
 helmcmd="$(helm)"
@@ -15,7 +15,7 @@ if [[ -z $helmcmd ]]; then
     curl https://raw.githubusercontent.com/msazurestackworkloads/kubetools/eventrouter/applications/common/install_helm.sh | bash
 else
     #Installing eventrouter to send input to stdout
-    echo -e  "$(date) [Info] Checking if event router is already deployed"
+    echo -e  "$(date) [Info] Checking if eventrouter is already deployed"
     
     #Checking if eventrouter is deployed
     INSTALL_STATUS=$(helm ls -d -r | grep 'DEPLOYED\(.*\)eventrouter' | grep -Eo '^[a-z,-]+')
@@ -26,9 +26,9 @@ else
         if helm install stable/eventrouter --set sink=stdout;  then
             echo -e  "${GREEN}$(date) [Info] Eventrouter installing sucessfully"
         else
-            echo -e  "${RED}$(date) [Err] Eventrouter Failed to install"
+            echo -e  "${RED}$(date) [Err] Eventrouter failed to install"
         fi 
     else
-        echo -e  "${GREEN}$(date) [Info] App(eventrouter) already installed"
+        echo -e  "${GREEN}$(date) [Info] App (eventrouter) already installed"
     fi
 fi
