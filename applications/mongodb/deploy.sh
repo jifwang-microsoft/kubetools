2363626e2fb218685f875a33363031762492ad9b
Bhuvaneswari Santharam <31025851+Bhuvaneswari-Santharam@users.noreply.github.com>
2019-06-18 14:47:42 -0700

    Create deploy.sh

---
+++ applications/mongodb/deploy.sh
@@ -0,0 +1,111 @@
+#!/bin/bash -e
+
+FILE_NAME=$0
+
+SCRIPT_FOLDER="$(dirname $FILE_NAME)"
+GIT_REPROSITORY="${GIT_REPROSITORY:-msazurestackworkloads/kubetools}"
+GIT_BRANCH="${GIT_BRANCH:-master}"
+COMMON_SCRIPT_FILENAME="common.sh"
+
+# Download common script file.
+curl -o $SCRIPT_FOLDER/$COMMON_SCRIPT_FILENAME \
+https://raw.githubusercontent.com/$GIT_REPROSITORY/$GIT_BRANCH/applications/common/$COMMON_SCRIPT_FILENAME
+if [ ! -f $SCRIPT_FOLDER/$COMMON_SCRIPT_FILENAME ]; then
+    log_level -e "File($COMMON_SCRIPT_FILENAME) failed to download."
+    exit 1
+fi
+
+source $SCRIPT_FOLDER/$COMMON_SCRIPT_FILENAME
+###########################################################################################################
+# The function will read parameters and populate below global variables.
+# IDENTITY_FILE, MASTER_IP, OUTPUT_SUMMARYFILE, USER_NAME
+parse_commandline_arguments $@
+
+log_level -i "------------------------------------------------------------------------"
+log_level -i "                Input Parameters"
+log_level -i "------------------------------------------------------------------------"
+log_level -i "IDENTITY_FILE       : $IDENTITY_FILE"
+log_level -i "MASTER_IP           : $MASTER_IP"
+log_level -i "OUTPUT_SUMMARYFILE  : $OUTPUT_SUMMARYFILE"
+log_level -i "USER_NAME           : $USER_NAME"
+log_level -i "------------------------------------------------------------------------"
+
+if [[ -z "$IDENTITY_FILE" ]] || \
+[[ -z "$MASTER_IP" ]] || \
+[[ -z "$USER_NAME" ]] || \
+[[ -z "$OUTPUT_SUMMARYFILE" ]]; then
+    log_level -e "One of mandatory parameter is not passed correctly."
+    print_usage
+    exit 1
+fi
+
+###########################################################################################################
+# Define all inner varaibles.
+OUTPUT_FOLDER="$(dirname $OUTPUT_SUMMARYFILE)"
+LOG_FILENAME="$OUTPUT_FOLDER/deploy.log"
+touch $LOG_FILENAME
+
+{
+    HELM_INSTALL_FILENAME="install_helm.sh"
+    APPLICATION_NAME="tomcat"
+    TEST_FOLDER="/home/$USER_NAME/$APPLICATION_NAME"
+    APPLICATION_FOLDER="applications/common"
+    
+    log_level -i "------------------------------------------------------------------------"
+    log_level -i "                Inner Variables"
+    log_level -i "------------------------------------------------------------------------"
+    log_level -i "APPLICATION_FOLDER          : $APPLICATION_FOLDER"
+    log_level -i "APPLICATION_NAME            : $APPLICATION_NAME"
+    log_level -i "HELM_INSTALL_FILENAME       : $HELM_INSTALL_FILENAME"
+    log_level -i "GIT_BRANCH                  : $GIT_BRANCH"
+    log_level -i "GIT_REPROSITORY             : $GIT_REPROSITORY"
+    log_level -i "TEST_FOLDER                 : $TEST_FOLDER"
+    log_level -i "------------------------------------------------------------------------"
+    
+    ###########################################################################################################
+    download_file_locally $GIT_REPROSITORY $GIT_BRANCH \
+    $APPLICATION_FOLDER \
+    $SCRIPT_FOLDER \
+    $HELM_INSTALL_FILENAME
+    
+    if [[ $? != 0 ]]; then
+        log_level -e "Download of file($HELM_INSTALL_FILENAME) failed."
+        printf '{"result":"%s","error":"%s"}\n' "failed" "Download of file($HELM_INSTALL_FILENAME) was not successfull." > $OUTPUT_SUMMARYFILE
+        exit 1
+    fi
+    
+    log_level -i "Create test folder($TEST_FOLDER)"
+    ssh -t -i $IDENTITY_FILE $USER_NAME@$MASTER_IP "mkdir -p $TEST_FOLDER"
+    
+    log_level -i "Copy file($HELM_INSTALL_FILENAME) to VM."
+    scp -i $IDENTITY_FILE \
+    $SCRIPT_FOLDER/$HELM_INSTALL_FILENAME \
+    $USER_NAME@$MASTER_IP:$TEST_FOLDER/
+    
+    # Install Helm chart
+    install_helm_chart $IDENTITY_FILE \
+        $USER_NAME \
+        $MASTER_IP \
+        $TEST_FOLDER \
+    $HELM_INSTALL_FILENAME
+    
+    if [[ $? != 0 ]]; then
+        printf '{"result":"%s","error":"%s"}\n' "failed" "Installing Helm was not successfull." > $OUTPUT_SUMMARYFILE
+        exit 1
+    fi
+    
+    install_helm_app $IDENTITY_FILE \
+        $USER_NAME \
+        $MASTER_IP \
+    $APPLICATION_NAME
+    
+    if [[ $? != 0 ]]; then
+        printf '{"result":"%s","error":"%s"}\n' "failed" "Installing App($APPLICATION_NAME) was not successfull." > $OUTPUT_SUMMARYFILE
+        exit 1
+    else
+        printf '{"result":"%s"}\n' "pass" > $OUTPUT_SUMMARYFILE
+    fi
+    
+    # Create result file, even if script ends with an error
+    #trap final_changes EXIT
+} 2>&1 | tee $LOG_FILENAME
